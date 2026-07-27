7a40be1d41131a2efa81ad3e467708d3f2471a96
nindgabeet <wenming.soh@gmail.com>
2026-07-27 11:22:00 +0800

    handled initial loading of env db string for the dev environment

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sohWenMing/aws_server
 
 go 1.26.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ internal/envloader/envloader.go
@@ -0,0 +1,60 @@
+package envloader
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type EnvSettings struct {
+	dbString string
+}
+
+func (e *EnvSettings) GetDBString() string {
+	return e.dbString
+}
+func (e *EnvSettings) SetDBString(env string) error {
+	if env == "DEV" {
+		dbString := os.Getenv("DBSTRING")
+		if dbString == "" {
+			return errors.New("dbstring could not be found for setting in dev environment")
+		}
+		e.dbString = dbString
+		return nil
+	}
+	if env == "PROD" {
+		e.dbString = "TODO"
+		return nil
+	}
+	return errors.New("unrecognized environment in SetDBString")
+}
+
+func LoadEnvironment(envPath string) (envSettings *EnvSettings, err error) {
+	envSettings = &EnvSettings{}
+	env := os.Getenv("ENV")
+	if env != "DEV" && env != "PROD" {
+		return nil, fmt.Errorf("%q is not a recognized environment\n", env)
+
+	}
+	if env == "DEV" {
+		err := loadEnvFile(envPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+	err = envSettings.SetDBString(env)
+	if err != nil {
+		return nil, err
+	}
+	return envSettings, nil
+}
+
+func loadEnvFile(envPath string) error {
+	err := godotenv.Load(envPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
